ceeb01e9e05ec06361370219183536d17db9ec57
vmorsell <viktor@upper.st>
2021-09-01 11:15:07 +0000

    Deploying to gh-pages from  @ d736f0ba4152b27cc069beabc883eca0c1957680 🚀

---
+++ _next/static/0DO6n5ldaQQka-SgGmeOR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/822-d8194801b34c009bdd9a.js","static/chunks/pages/index-434e29181a953bf65ee2.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
