a5b57dd8601af008ab9fdd001abf7ea4054becf3
Viktor Mörsell <viktor@upper.st>
2021-09-01 13:45:13 +0200

    Misc layout improvements (#8)
    * Add show section title boolean
    
    * Don't purge styles
    
    * Add container horizontal padding
    
    * Enlarge blockquote text

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      padding: '2em',
+    },
     extend: {},
   },
   variants: {
